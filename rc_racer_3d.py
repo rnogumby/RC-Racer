4b9bf28c092cf3704899a2f2a99736c1a33b1a9b
rnogumby <gumbygumberson85@gmail.com>
2025-08-13 19:29:16 -0500

    sync

--- rc_racer_3d.py
+++ rc_racer_3d.py
@@ -6,7 +6,7 @@ app = Ursina()
 start_screen = Entity(parent=camera.ui, model='quad', scale=(1.5,0.5), color=color.black, position=(0,0,0), texture=None)
 start_text = Text(text='RC Racer 3D\nPress SPACE to start', origin=(0,0), scale=2, parent=start_screen, position=(0,0,0.1))
 
-car = None
+car = Ncar = Entity(model='cube', texture='nissan-skyline-gt-r.png', scale=(1,0.5,2), position=(0,0.5,0))
 track = None
 started = False
 speed = 0
@@ -17,8 +17,8 @@ def start_game():
     start_screen.enabled = False
     start_text.enabled = False
     # Create track and car
-    track = Entity(model='plane', scale=(20,1,20), color=color.gray, texture='white_cube', texture_scale=(20,20), position=(0,0,0))
-    car = Entity(model='cube', color=color.red, scale=(1,0.5,2), position=(0,0.5,0))
+    track = Entity(model='plane', scale=(20,1,20), texture='nissan-skyline-gt-r.png', texture_scale=(20,20), position=(0,0,0))
+    car = Entity(model='cube', texture='nissan-skyline-gt-r.png', scale=(1,0.5,2), position=(0,0.5,0))
     camera.position = (0, 15, -25)
     camera.look_at(car)
     started = True
